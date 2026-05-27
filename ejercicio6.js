1dde31560e152a7873ac17dd131c0c127bf684b8
Victor Cadavid <victorcadavid16@gmail.com>
2026-05-26 21:51:22 -0500

    feat:acabo de agregar el ejercicio6 donde solicita Crea un programa que: genere un número aleatorio entre 1 y 20

---
+++ ejercicio6.js
@@ -0,0 +1,40 @@
+// Ejercicio 6 – Juego del adivinador
+
+// Generamos un número aleatorio entre 1 y 20
+let numeroSecreto = Math.floor(Math.random() * 20) + 1;
+
+// Variable para guardar el intento del usuario
+let intento = 0;
+
+// Variable para controlar el ciclo
+let adivinado = false;
+
+// Ciclo que se repetirá hasta que el usuario adivine el número
+while (adivinado === false) {
+
+    // Pedimos un número al usuario
+    intento = parseInt(prompt("Adivina el número entre 1 y 20"));
+
+    // Condicional:
+    // Se usa para verificar si el número ingresado es igual al número secreto.
+    if (intento === numeroSecreto) {
+
+        // Este bloque se ejecuta si el usuario adivina correctamente
+        alert("¡Felicidades! Adivinaste el número.");
+
+        // Cambiamos la variable para terminar el ciclo
+        adivinado = true;
+
+    } else if (intento < numeroSecreto) {
+
+        // Este bloque se ejecuta si el número ingresado es menor
+        // que el número secreto.
+        alert("Incorrecto. El número secreto es MAYOR.");
+
+    } else {
+
+        // Este bloque se ejecuta si el número ingresado es mayor
+        // que el número secreto.
+        alert("Incorrecto. El número secreto es MENOR.");
+    }
+}
